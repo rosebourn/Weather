8451c2131ff27aff7447c85def3c0896d4659f7f
mianak78 <rosebourn@gmail.com>
2019-10-18 17:17:13 -0600

    api search working

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-var search = document.querySelector("input-group");
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=4785a78b216636ca439247db2c4751b1";
 
-
-
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-  console.log(queryURL);
-  console.log(response);
+$("#search").on("click", function(event) {
+    event.preventDefault();
+    var search = $("#city").val();
+    console.log(search);
+weather(search);
+  
 })
 
-/*var cityDiv = $("<div class='city'>");
-  var */
+function weather(search) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&APPID=4785a78b216636ca439247db2c4751b1";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+      console.log(response);
+
+
+    })
+}
+
+
+
+
